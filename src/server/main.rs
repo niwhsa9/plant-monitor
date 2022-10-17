5d6fd63399075371e129f200f8e20539e158462b
Ashwin Gupta <ashwingupta2000@gmail.com>
2022-10-16 21:30:32 -0400

    issues with compiling deps for WASM

--- src/server/main.rs
+++ src/server/main.rs
@@ -1,3 +1,11 @@
-fn main() {
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
     println!("Hello from server!");
+    let routes = warp::any().map(|| {
+        println!("get req");
+         "Hello World"
+    });
+    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
