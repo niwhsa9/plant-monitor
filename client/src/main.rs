8cdb5e5da47036a958d6199344af8311e0f5db25
Ashwin Gupta <ashwingupta2000@gmail.com>
2022-10-17 23:46:30 -0400

    debug http get issues

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,4 +1,6 @@
 use yew::prelude::*;
+use wasm_bindgen::prelude::*;
+use wasm_bindgen_futures::spawn_local;
 
 #[function_component(App)]
 fn app() -> Html {
@@ -7,6 +9,27 @@ fn app() -> Html {
     }
 }
 
+//#[wasm_bindgen]
 fn main() {
+    wasm_logger::init(wasm_logger::Config::default());
+    log::info!("Some info");
+
+    println!("trying to spawn local");
+    spawn_local( run() );
     yew::start_app::<App>();
+    log::info!("Some info");
+}
+
+#[wasm_bindgen]
+pub async fn run() {
+    log::info!("here");
+    //let client = reqwest::Client::new();
+    let res = reqwest::get("/api").await;//client.get("localhost:3030").send().await;
+    match res {
+        Ok(_) => log::info!("good"),
+        Err(_) => log::error!("bad")
+    };
+    log::info!("done");
+
+
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,11 +1,12 @@
-use warp::Filter;
+use warp::{Filter, http::Response};
 
 #[tokio::main]
 async fn main() {
     println!("Hello from server!");
     let routes = warp::any().map(|| {
         println!("get req");
-         "Hello World"
+        //#Response::builder().header("test", "val").body("lol")
+        "hello world"
     });
     warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
