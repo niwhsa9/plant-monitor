30ee53a2588cef28a8eeb201930155d37f55b4f0
ashwin <ashwingupta2000@gmail.com>
2022-12-18 20:25:35 -0500

    refactor for yew 0.20

--- client/src/main.rs
+++ client/src/main.rs
@@ -3,33 +3,43 @@ use wasm_bindgen::prelude::*;
 use wasm_bindgen_futures::spawn_local;
 use reqwasm::http::*;
 
-pub struct Dashboard {
-    num_widgets : u32,
+/*
+ * Plant Display Widget
+ */
+#[derive(Properties, PartialEq)]
+pub struct PlantWidgetProps {
+    pub name: String,
 }
 
-// TODO: change this to regular Component and do server transaction in create() to avoid repeat
-#[function_component(App)]
-fn app() -> Html {
-    // Initial request to determine the number of widgets
-    let v = vec![1, 2, 3, 4, 5];
+#[function_component]
+fn PlantWidget(props : &PlantWidgetProps) -> Html {
+    log::info!("Widget rerender");
     html! {
-        <> 
-        {v.into_iter().map(|_id| {html!{ <PlantWidget/>} } ).collect::<Html>()}
-        </>
+        <div class="plant-widget">
+            <h1>{&props.name}</h1>
+        </div>
     }
 }
 
-pub struct PlantWidget {
-    name : String  
+/*
+ * Top Level Application Dashboard
+ */
+pub struct Dashboard {
+    num_plants : u32,
+    plants : Vec<PlantWidgetProps>
 }
 
-impl Component for PlantWidget {
+impl Component for Dashboard {
     type Message = ();
     type Properties = ();
 
     fn create(ctx : &Context<Self>) -> Self {
-        log::info!("Widget create");
-        Self{name : String::from("lol")}
+        // Load application information from server 
+        let plant_widgets = vec![ 
+            PlantWidgetProps{name : String::from("Claude")}, 
+            PlantWidgetProps{ name : String::from("Jacobi")} 
+        ];
+        Self{num_plants : 0, plants : plant_widgets}
     }
 
     fn update(&mut self, ctx : &Context<Self>, msg: Self::Message) -> bool {
@@ -42,19 +52,23 @@ impl Component for PlantWidget {
         // Get the resource URL for the image tag
     
         log::info!("Widget rerender");
+        let v = vec![1, 2, 3, 4, 5];
         html! {
-            <div class="plant-widget">
-                <h1>{&self.name}</h1>
-            </div>
+            <> 
+            {v.into_iter().map(|_id| {html!{ <PlantWidget name="Plant 1"/>} } ).collect::<Html>()}
+            </>
         }
     }
+
 }
+
 
 //#[wasm_bindgen]
 fn main() {
     wasm_logger::init(wasm_logger::Config::default());
     spawn_local( run() );
-    yew::start_app::<App>();
+    yew::Renderer::<Dashboard>::new().render();
+
 }
 
 #[wasm_bindgen]
