935ab281253b4321a89e6bcf3e77a2938de8287a
ashwin <ashwingupta2000@gmail.com>
2022-12-20 19:09:59 -0500

    update fn

--- client/src/main.rs
+++ client/src/main.rs
@@ -25,23 +25,55 @@ fn PlantWidget(props : &PlantWidgetProps) -> Html {
  * Top Level Application Dashboard
  */
 pub struct Dashboard {
-    plants : Vec<PlantWidgetProps>
+    loading : Box<bool>,
+    plants : Vec<PlantWidgetProps>,
+}
+
+pub enum DashboardMsg {
+    DataReady()
+}
+impl Dashboard {
+    // Retrieves plant data from the server for the application 
+    // and issues callback on completion
+    fn get_plant_data(completed_cb : Callback<(), ()>) {
+        spawn_local( async move { 
+            // HTTP GET data from server
+
+            // Signal completion
+            completed_cb.emit(());      
+        });
+    }
 }
 
 impl Component for Dashboard {
-    type Message = ();
+    type Message = DashboardMsg;
     type Properties = ();
+
+    fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            Self::Message::DataReady() => {
+
+            }
+        }
+        false
+    }
 
     fn create(ctx : &Context<Self>) -> Self {
         // Load application information from server 
-
+        let loading = Box::new(true);
+        let mut l2 = loading.clone();
+        let q = async move { 
+            *l2 = false; 
+            
+        };
+        spawn_local( q );
         // Request an image from the API for the particular plant
         // Get the resource URL for the image tag
         let plant_widgets = vec![ 
             PlantWidgetProps{name : String::from("Claude")}, 
             PlantWidgetProps{ name : String::from("Jacobi")} 
         ];
-        Self{plants : plant_widgets}
+        Self{loading : Box::new(true), plants : plant_widgets}
     }
 
     fn view(&self, ctx: &Context<Self>) -> Html {
@@ -57,6 +89,7 @@ impl Component for Dashboard {
 
 fn main() {
     wasm_logger::init(wasm_logger::Config::default());
+    // Get the data we need from the server, it will callback 
     spawn_local( run() );
     yew::Renderer::<Dashboard>::new().render();
 
