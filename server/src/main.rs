8a4597bc9fc76a1c0ca02cd55226b556fb355df4
ashwin <ashwingupta2000@gmail.com>
2022-12-24 21:05:40 -0500

    build data vec on server from db

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,14 +1,15 @@
+use std::{rc::Rc, sync::Arc};
+
 use warp::{Filter};
 use messages::msg::{Point, PlantData};
 use chrono::{DateTime, Utc, TimeZone, NaiveDate};
 
-mod handlers;
-
 #[tokio::main]
 async fn main() {
     println!("Server starting");
+    let mut data = Vec::<PlantData>::new();
 
-    // Issue database transaction and cache
+    // Issue database transactions and cache
     let connection = sqlite::open("plants.db").unwrap();
     let query = "
         CREATE TABLE IF NOT EXISTS plants (name TEXT, img TEXT, water DATETIME, PRIMARY KEY (name));
@@ -27,44 +28,23 @@ async fn main() {
         let e = PlantData {
             name : String::from(row.read::<&str, _>("name")),
             img_path : String::from(row.read::<&str, _>("img")),
-            last_water_time : Utc.datetime_from_str(row.read::<&str, _>("water"), "").unwrap(),
+            last_water_time : Utc.datetime_from_str(
+                row.read::<&str, _>("water"), "%Y-%m-%d %H:%M:%S").unwrap(),
         };
-        println!("name = {}", row.read::<&str, _>("name"));
-        println!("water = {}", row.read::<&str, _>("img"));
-        println!("water = {}", row.read::<&str, _>("water"));
+        data.push(e);
     }
-
+    let data_ptr = Arc::new(data);
 
     let data_route = 
-        warp::path("plant_data").map( || { 
-            // [Temp] construct vector of Plant Data
-            let naivedatetime_utc = NaiveDate::from_ymd_opt(2000, 1, 12).unwrap().and_hms_opt(2, 0, 0).unwrap();
-            let datetime_utc = DateTime::<Utc>::from_utc(naivedatetime_utc, Utc); 
-            let cur_time: DateTime<Utc> = Utc::now();
-            let p = vec![
-                PlantData{ name : String::from("Wernher"), img_path : String::from("/api/img/plant.jpg"), last_water_time : cur_time},
-                PlantData{ name : String::from("Wernher1"), img_path : String::from("/api/img/plant.jpg"), last_water_time : datetime_utc},
-                PlantData{ name : String::from("Wernher2"), img_path : String::from("/api/img/plant.jpg"), last_water_time : cur_time},
-                PlantData{ name : String::from("Wernher3"), img_path : String::from("/api/img/plant.jpg"), last_water_time : cur_time},
-            ];
-            // Return data
-            warp::reply::json(&p)
+        warp::path("plant_data").map( move || { 
+            warp::reply::json(&*data_ptr)
             }
         );
     let image_route = warp::path("img") .and(warp::fs::dir("temp"));
 
-    let test_route = 
-        warp::path("test").map( || {
-            println!("Test triggered");
-            //"Test Reply" 
-            let p = Point{ x: 1, y: 2};
-            warp::reply::json(&p)
-        });
-
     let routes = 
         warp::get().and(
             data_route
-            .or(test_route)
             .or(image_route)
         );
 
