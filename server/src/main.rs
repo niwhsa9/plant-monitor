4b9e7e4fe826f96bf434dd4871a989533fe018fc
Ashwin Gupta <ashwingupta2000@gmail.com>
2023-01-24 18:03:10 -0500

    autoformat server code

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,9 +1,9 @@
-use std::{sync::Arc, sync::Mutex, borrow::BorrowMut, path::Path, fs};
-use warp::{Filter, multipart, hyper::Uri};
-use messages::msg::{PlantData};
-use chrono::{Utc, TimeZone};
 use bytes::BufMut;
+use chrono::{TimeZone, Utc};
 use futures_util::{TryFutureExt, TryStreamExt};
+use messages::msg::PlantData;
+use std::{borrow::BorrowMut, fs, path::Path, sync::Arc, sync::Mutex};
+use warp::{hyper::Uri, multipart, Filter};
 
 const FORMAT: &'static str = "%Y-%m-%d %H:%M:%S";
 
@@ -22,96 +22,100 @@ async fn main() {
     connection.lock().unwrap().execute(query).unwrap();
 
     let query = "SELECT * from plants";
-    for row in connection.lock().unwrap()
+    for row in connection
+        .lock()
+        .unwrap()
         .prepare(query)
         .unwrap()
         .into_iter()
         .map(|row| row.unwrap())
     {
         let e = PlantData {
-            name : String::from(row.read::<&str, _>("name")),
-            img_path : String::from(row.read::<&str, _>("img")),
-            last_water_time : Utc.datetime_from_str(
-                row.read::<&str, _>("water"), FORMAT).unwrap(),
+            name: String::from(row.read::<&str, _>("name")),
+            img_path: String::from(row.read::<&str, _>("img")),
+            last_water_time: Utc
+                .datetime_from_str(row.read::<&str, _>("water"), FORMAT)
+                .unwrap(),
         };
         data.push(e);
     }
     let data_ptr = Arc::new(Mutex::new(data));
     let data_ptr_clone = data_ptr.clone();
     // Return data about all available plants
-    let data_route = 
-        warp::path("plant_data").map( move || { 
-            warp::reply::json(&*(data_ptr.lock().unwrap()))
-            }
-        );
-    let image_route = warp::path("img") .and(warp::fs::dir("temp"));
+    let data_route =
+        warp::path("plant_data").map(move || warp::reply::json(&*(data_ptr.lock().unwrap())));
+    let image_route = warp::path("img").and(warp::fs::dir("temp"));
 
     // Handle a request to reset watering time of a plant
-    let reset_time_route = 
-       warp::path("reset_time").
-       and(warp::path::param::<String>()).
-       map(move|name : String| {
-        let new_time = Utc::now();
-        let new_time_str = new_time.format(FORMAT);
+    let reset_time_route = warp::path("reset_time")
+        .and(warp::path::param::<String>())
+        .map(move |name: String| {
+            let new_time = Utc::now();
+            let new_time_str = new_time.format(FORMAT);
 
-        // Update cache
-        let mut data = data_ptr_clone.lock().unwrap();
-        let mut i = data.iter_mut();
-        let res = i.find(|x| x.name == name ).unwrap();
-        res.last_water_time = new_time;
+            // Update cache
+            let mut data = data_ptr_clone.lock().unwrap();
+            let mut i = data.iter_mut();
+            let res = i.find(|x| x.name == name).unwrap();
+            res.last_water_time = new_time;
 
-        // Write through to disk as well
-        let query = format!("
+            // Write through to disk as well
+            let query = format!(
+                "
             UPDATE plants SET water='{}' where name='{}'; ",
-            new_time_str, name
-        );
-        connection.lock().unwrap().execute(query).unwrap();
-        warp::reply()
+                new_time_str, name
+            );
+            connection.lock().unwrap().execute(query).unwrap();
+            warp::reply()
         });
 
     // Add new plant form endpoint
-    
-    let new_plant = multipart::form().and_then(|form: multipart::FormData| {
+
+    let new_plant = multipart::form()
+        .and_then(|form: multipart::FormData| {
             async {
-            // Collect the fields into (name, value): (String, Vec<u8>), taken from warp examples
-            let part: Result<Vec<(String, Vec<u8>)>, warp::Rejection> = form
-                .and_then(|part| {
-                    let name = part.name().to_string();
-                    let value = part.stream().try_fold(Vec::new(), |mut vec, data| {
-                        vec.put(data);
-                        async move { Ok(vec) }
+                // Collect the fields into (name, value): (String, Vec<u8>), taken from warp examples
+                let part: Result<Vec<(String, Vec<u8>)>, warp::Rejection> = form
+                    .and_then(|part| {
+                        let name = part.name().to_string();
+                        let value = part.stream().try_fold(Vec::new(), |mut vec, data| {
+                            vec.put(data);
+                            async move { Ok(vec) }
+                        });
+                        value.map_ok(move |vec| (name, vec))
+                    })
+                    .try_collect()
+                    .await
+                    .map_err(|e| {
+                        panic!("multipart error: {:?}", e);
                     });
-                    value.map_ok(move |vec| (name, vec))
-                })
-                .try_collect()
-                .await
-                .map_err(|e| {
-                    panic!("multipart error: {:?}", e);
-                });
-            part
+                part
             }
-        }).map(|v : Vec<(String, Vec<u8>)>| { 
+        })
+        .map(|v: Vec<(String, Vec<u8>)>| {
             for p in v.iter() {
-                let (name, data) = p; 
+                let (name, data) = p;
                 match name.as_str() {
                     "plant_name" => {
-                        println!("recieved {} {}", name, String::from_utf8(data.to_vec()).unwrap());
-                    },
+                        println!(
+                            "recieved {} {}",
+                            name,
+                            String::from_utf8(data.to_vec()).unwrap()
+                        );
+                    }
                     "fname" => {
                         println!("img size {}", data.len());
-                        //image::save_buffer("image.jpg", data, 500, 500, image::ColorType::Rgb8).unwrap()
                         let path = Path::new("image.jpg");
                         fs::write(path, data).unwrap();
                     }
-                    _ => panic!()
+                    _ => panic!(),
                 }
-                //println!("recieved {} {}", name, String::from_utf8(data.to_vec()).unwrap());
             }
             warp::reply()
         });
 
-    // Serve API endpoints    
-    let routes = 
+    // Serve API endpoints
+    let routes =
         //warp::body::content_length_limit(100000).and
         (warp::get().and(
             data_route
