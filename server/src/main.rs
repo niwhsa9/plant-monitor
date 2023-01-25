b8dbddf909bb7f657c314d6ee01af5fcf976d48b
Ashwin Gupta <ashwingupta2000@gmail.com>
2023-01-24 21:21:13 -0500

    server side code to support new plants

--- server/src/main.rs
+++ server/src/main.rs
@@ -16,8 +16,8 @@ async fn main() {
     let connection = Arc::new(Mutex::new(sqlite::open("plants.db").unwrap()));
     let query = "
         CREATE TABLE IF NOT EXISTS plants (name TEXT, img TEXT, water DATETIME, PRIMARY KEY (name));
-        INSERT OR IGNORE INTO plants VALUES ('Claude', '/api/img/plant.jpg', '2022-09-24 09:05:00' );
-        INSERT OR IGNORE INTO plants VALUES ('Jacobi', '/api/img/plant.jpg', '2022-09-25 09:05:00' );
+        INSERT OR IGNORE INTO plants VALUES ('Claude', '/api/image/plant.jpg', '2022-09-24 09:05:00' );
+        INSERT OR IGNORE INTO plants VALUES ('Jacobi', '/api/image/plant.jpg', '2022-09-25 09:05:00' );
     ";
     connection.lock().unwrap().execute(query).unwrap();
 
@@ -44,10 +44,13 @@ async fn main() {
     // Return data about all available plants
     let data_route =
         warp::path("plant_data").map(move || warp::reply::json(&*(data_ptr.lock().unwrap())));
-    let image_route = warp::path("img").and(warp::fs::dir("temp"));
+    let image_route = warp::path("image").and(warp::fs::dir("image"));
 
     // Handle a request to reset watering time of a plant
-    let reset_time_route = warp::path("reset_time")
+    let reset_time_route = {
+        let connection = connection.clone();
+        let data_ptr_clone = data_ptr_clone.clone();
+        warp::path("reset_time")
         .and(warp::path::param::<String>())
         .map(move |name: String| {
             let new_time = Utc::now();
@@ -67,11 +70,14 @@ async fn main() {
             );
             connection.lock().unwrap().execute(query).unwrap();
             warp::reply()
-        });
+        })
+    };
 
     // Add new plant form endpoint
 
-    let new_plant = multipart::form()
+    let new_plant = {
+        let connection = connection.clone();
+        multipart::form()
         .and_then(|form: multipart::FormData| {
             async {
                 // Collect the fields into (name, value): (String, Vec<u8>), taken from warp examples
@@ -92,11 +98,18 @@ async fn main() {
                 part
             }
         })
-        .map(|v: Vec<(String, Vec<u8>)>| {
+        .map(move |v: Vec<(String, Vec<u8>)>| {
+            let time = Utc::now();
+            // TODO: these should probably be Option<String>
+            let mut plant_name = String::default();
+            let mut file_name = String::default();
+            let data_ptr_clone = data_ptr_clone.clone();
             for p in v.iter() {
                 let (name, data) = p;
                 match name.as_str() {
                     "plant_name" => {
+                        plant_name = String::from_utf8(data.to_vec()).unwrap();
+                        
                         println!(
                             "recieved {} {}",
                             name,
@@ -105,14 +118,30 @@ async fn main() {
                     }
                     "fname" => {
                         println!("img size {}", data.len());
-                        let path = Path::new("image.jpg");
+                        file_name = format!("image/{}.jpg", time.timestamp_millis());
+                        let path = Path::new(&file_name);
                         fs::write(path, data).unwrap();
+                        file_name = "/api/" .to_owned()+ &file_name;
                     }
                     _ => panic!(),
                 }
             }
+
+            let query = format!("
+                INSERT OR IGNORE INTO plants VALUES ('{}', '{}', '{}' );
+            ", plant_name, file_name, time.format(FORMAT));
+
+            let mut data = data_ptr_clone.lock().unwrap();
+            data.push(PlantData{
+                name : plant_name,
+                img_path : file_name, 
+                last_water_time: time
+            });
+            
+            connection.lock().unwrap().execute(query).unwrap();
             warp::reply()
-        });
+        })
+    };
 
     // Serve API endpoints
     let routes =
